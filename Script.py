1f977f8359e5c0b1b055270287c6f276ec8ea588
Nassimha <nassimtout24@gmail.com>
2024-07-13 09:57:49 +0100

    Update Script.py

--- Script.py
+++ Script.py
@@ -4,7 +4,7 @@ from google.oauth2.credentials import Credentials
 from google.auth.transport.requests import Request
 
 def main():
-    rss_url = 'https://rss.app/feeds/RAU0UmLGELQ6vdB9.xml'  # ضع هنا URL لـ RSS Feed الخاص بموقعك
+    rss_url = 'http://fetchrss.com/rss/66924131383244f7540a82546692410109547ffc7409ebb3.xml'  # ضع هنا URL لـ RSS Feed الخاص بموقعك
     feed = feedparser.parse(rss_url)
 
     # طباعة عنوان الـ Feed
