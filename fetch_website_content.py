78e1bc8e49ec0a56dcfbfe54e8adfa0ce7cd5310
Nassimha <nassimtout24@gmail.com>
2024-07-18 09:42:34 +0100

    Update fetch_website_content.py

--- fetch_website_content.py
+++ fetch_website_content.py
@@ -2,7 +2,7 @@ import os
 import requests
 
 # إعداد URL موقع الويب
-WEBSITE_URL = os.getenv('https://like-manga.net/')
+WEBSITE_URL = os.getenv('WEBSITE_URL')
 
 # دالة لجلب المحتوى من موقع الويب
 def fetch_website_content():
