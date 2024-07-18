ff2cb93c66633f21f3e5814192033c5020814c96
Nassimha <nassimtout24@gmail.com>
2024-07-18 09:03:59 +0100

    Update and rename Script.py to fetch_website_content().py

---
+++ fetch_website_content().py
@@ -0,0 +1,17 @@
+import os
+import requests
+
+# إعداد URL موقع الويب
+WEBSITE_URL = os.getenv('https://like-manga.net/feed')
+
+# دالة لجلب المحتوى من موقع الويب
+def fetch_website_content():
+    response = requests.get(WEBSITE_URL)
+    response.raise_for_status()
+    return response.text
+
+# حفظ المحتوى في ملف
+if __name__ == '__main__':
+    content = fetch_website_content()
+    with open('website_content.html', 'w') as file:
+        file.write(content)
